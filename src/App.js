2558b9a21a4deb0693cfad6d08276cb3f1a4ab44
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-02-18 20:22:58 +0530

    changes are done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import About from './components/About';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import Socials from './components/Socials';
 
 function App() {
   return (
 <div>
   <NavBar/>
   <Home/>
+  <Socials/>
+  <About/>
 </div>
   );
 }
